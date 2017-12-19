2a3c85d011450cf5e114da62e0690f3f0bf9d669
Ray Villalobos <ray@planetoftheweb.com>
2017-12-19 17:49:26 -0500

    Wyatt is moving with keyboard

--- docs/js/game.js
+++ docs/js/game.js
@@ -1,21 +1,49 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(600, 800, Phaser.AUTO, 'invasion', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-  game.load.atlasJSONHash('bot', './images/wyatt_cycle.png', './images/wyatt_cycle.json');
+  game.load.atlasJSONHash('wyatt', './images/wyatt_cycle.png', './images/wyatt_cycle.json');
 }
+
+var cursors, wyatt;
 
 function create() {
 
   //  This sprite is using a texture atlas for all of its animation data
-  var bot = game.add.sprite(200, 200, 'bot');
+  wyatt = game.add.sprite(game.world.centerX, game.world.centerY, 'wyatt');
+  wyatt.anchor.set(0.5);
+
+  cursors = game.input.keyboard.createCursorKeys();
+  var text = game.add.text(32, 32, 'Cursors to move. Shift + Up / Down to Rotate World', { fill: '#ffffff' });
 
   //  Here we add a new animation called 'run'
   //  We haven't specified any frames because it's using every frame in the texture atlas
-  bot.animations.add('run');
+  wyatt.animations.add('still', [0]);
+  wyatt.animations.add('walk-down', [0, 4, 0, 5]);
+  wyatt.animations.add('walk-left', [1, 2, 1, 3]);
+  wyatt.animations.add('walk-right', [6, 7, 6, 8]);
+  wyatt.animations.add('walk-up', [9, 10, 9, 11]);
+}
 
-  //  And this starts the animation playing by using its key ("run")
-  //  15 is the frame rate (15fps)
-  //  true means it will loop when it finishes
-  bot.animations.play('run', 15, true);
+function update() {
+  if (cursors.up.isDown) {
+    wyatt.animations.play('walk-up', 8, true);
+    wyatt.y -= 2;
+  } else if (cursors.down.isDown) {
+    wyatt.animations.play('walk-down', 8, true);
+    wyatt.y += 2;
+  } else if (cursors.left.isDown) {
+    wyatt.animations.play('walk-left', 8, true);
+    wyatt.x -= 2;
+  } else if (cursors.right.isDown) {
+    wyatt.animations.play('walk-right', 8, true);
+    wyatt.x += 2;
+  } else {
+    wyatt.animations.play('still', 8, true);
+  }
+}
+
+function render() {
+
+  game.debug.cameraInfo(game.camera, 32, 500);
 }
 //# sourceMappingURL=game.js.map
